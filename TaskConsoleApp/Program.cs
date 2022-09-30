69f60320c5c744a8e6944d00579518f78fe7dae3
mfthdkr <50250549+mfthdkr@users.noreply.github.com>
2022-10-01 02:06:06 +0300

    Task.WhenAll

--- TaskConsoleApp/Program.cs
+++ TaskConsoleApp/Program.cs
@@ -6,21 +6,9 @@ namespace TaskConsoleApp
 {
     internal class Program
     {
-        public static void Calis(Task<string> data)
-        {   
-            // 100 satırlık kod
-            Console.WriteLine("data uzunluk : " + data.Result.Length);
-        }
-        static async Task Main(string[] args)
+        
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            var myTask= new HttpClient().GetStringAsync("https://google.com").ContinueWith(Calis);
-
-
-            Console.WriteLine("Arada yapılacak işlemeler");
-            await myTask;
-            
 
         }
     }
