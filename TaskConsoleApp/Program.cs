1dd6d21c67f1e97eccc13150b45d3043e5baa26c
mfthdkr <50250549+mfthdkr@users.noreply.github.com>
2022-10-01 02:31:58 +0300

    Task.WhenAny

--- TaskConsoleApp/Program.cs
+++ TaskConsoleApp/Program.cs
@@ -1,15 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TaskConsoleApp
 {
-    internal class Program
+    public class Content
     {
-        
-        static void Main(string[] args)
+        public string Site { get; set; }
+        public int Length { get; set; }
+    }
+    internal class Program
+    {   
+        static async Task Main(string[] args)
         {
+            Console.WriteLine("Main thread" + Thread.CurrentThread.ManagedThreadId);
+
+            List<string> urlList = new List<string>()
+            {
+                "https://www.google.com",
+                "https://www.microsoft.com",
+                "https://www.amazon.com",
+                "https://www.n11.com",
+                "https://www.haberturk.com",
+            };
+
+            List<Task<Content>> taskList= new List<Task<Content>>();
+            urlList.ForEach(x =>
+            {
+                taskList.Add(GetContentAsync(x));
+            });
+
+            var contents =  Task.WhenAll(taskList.ToArray());
+
+            Console.WriteLine("WhenAll'dan sonra başka işlemler.");
+
+            var data = await contents;
+
+            data.ToList().ForEach(x =>
+            {
+                Console.WriteLine($"{x.Site} - boyut: {x.Length} ");
+            });
 
         }
+        public static async Task<Content> GetContentAsync(string url)
+        {
+            Content content = new Content();
+            var data = await new HttpClient().GetStringAsync(url);
+
+            content.Site = url;
+            content.Length = data.Length;
+            Console.WriteLine("GetContentAsync thread" + Thread.CurrentThread.ManagedThreadId);
+
+            return content;
+        }
     }
+     
 }
