08503cdf68a5d5144209688e6b331ef13758b68e
mfthdkr <50250549+mfthdkr@users.noreply.github.com>
2022-10-01 23:25:09 +0300

    Parallel.ForEach

---
+++ ForEachParallelApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ForEachParallelApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string picturePath = @"C:\Users\MUHAMMET FATİH DİKER\Desktop\Pictures";
+
+            var files = Directory.GetFiles(picturePath);
+
+            Parallel.ForEach(files, (item) =>
+            {
+                Console.WriteLine("thread no: " + Thread.CurrentThread.ManagedThreadId);
+                Image img = new Bitmap(item);
+                var thumbnail = img.GetThumbnailImage(50, 50, () => false, IntPtr.Zero);
+
+                thumbnail.Save(Path.Combine(picturePath,"thumbnail",Path.GetFileName(item)));
+
+            });
+
+            Console.WriteLine("işlem bitti");
+        }
+    }
+}
