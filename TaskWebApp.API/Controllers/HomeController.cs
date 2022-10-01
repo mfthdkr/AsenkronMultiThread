ff2b8d074925af8a1ec182a6f15651c60792f4e6
mfthdkr <50250549+mfthdkr@users.noreply.github.com>
2022-10-01 16:17:44 +0300

    cancellationtoken.web

--- TaskWebApp.API/Controllers/HomeController.cs
+++ TaskWebApp.API/Controllers/HomeController.cs
@@ -1,27 +1,50 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace TaskWebApp.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class HomeController : ControllerBase
-    {
+    {   
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet]
-        public async Task<IActionResult> GetContentAsync()
-        {   
-            Thread.Sleep(5000);
-            var myTask = new HttpClient().GetStringAsync("https://www.google.com");
+        public async Task<IActionResult> GetContentAsync(CancellationToken token)
+        {
 
-            // başka işlemler
+            try
+            {
+                _logger.LogInformation("İstek başladı");
 
-            var data = await myTask;
+                await Task.Delay(3000, token);
+                var myTask = new HttpClient().GetStringAsync("https://www.google.com");
 
-            return Ok(data);
+                // başka işlemler
+
+                var data = await myTask;
+                _logger.LogInformation("İstek bitti");
+
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+               _logger.LogInformation("istek iptal edildi." +ex.Message);
+               return BadRequest();
+            }
+            
+
 
         }
     }
